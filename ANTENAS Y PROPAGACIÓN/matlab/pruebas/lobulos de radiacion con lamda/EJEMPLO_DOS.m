7b82070c51810e33b1cdafd66085d65028c20b8f
miyashiromc <mkmcmiyako@gmail.com>
2025-06-08 10:16:32 -0500

    TAREA LOBULOS DE RADIACION DIPOLO

---
+++ ANTENAS Y PROPAGACIÓN/matlab/pruebas/lobulos de radiacion con lamda/EJEMPLO_DOS.m
@@ -0,0 +1,45 @@
+% Dipole L = lambda - Patrón 3D y cortes H-plane y E-plane
+clc; clear; close all;
+
+% Parámetros
+f      = 2.6e9;
+c      = 3e8;
+lambda = c/f;
+L      = lambda;      % dipolo de longitud lambda
+k      = 2*pi/lambda;
+
+% Malla angular
+n     = 360;
+theta = linspace(0,2*pi,n);
+phi   = linspace(0,pi,n);
+[Theta,Phi] = meshgrid(theta,phi);
+
+% Patrón E(theta,phi) normalizado
+E = abs((cos(k*L/2.*cos(Phi)) - cos(k*L/2))./(sin(Phi)+eps));
+E = E / max(E(:));
+
+% Patrón 3D
+[X,Y,Z] = sph2cart(Theta,pi/2-Phi,E);
+figure;
+surf(X,Y,Z,E,'EdgeColor','none');
+axis equal; axis off;
+colormap jet; colorbar;
+view(40,30);
+lighting gouraud; camlight headlight;
+title('3D - Dipole L=lambda');
+
+% H-plane (phi = pi/2)
+[~,i] = min(abs(phi - pi/2));
+Eh = E(i,:);
+figure;
+polarplot(theta,Eh,'b','LineWidth',2);
+title('H-plane (phi=pi/2) - L=lambda');
+
+% E-plane (theta = 0)
+[~,j] = min(abs(theta - 0));
+Ev = E(:,j);
+phi_full = [phi, phi+pi];
+Ev_full  = [Ev.', Ev.'];
+figure;
+polarplot(phi_full,Ev_full,'r','LineWidth',2);
+title('E-plane (theta=0) - L=lambda');
---
+++ ANTENAS Y PROPAGACIÓN/matlab/pruebas/lobulos de radiacion con lamda/EJEMPLO_UNO.m
@@ -0,0 +1,45 @@
+% Dipole L = lambda/2 - Patrón 3D y cortes H-plane y E-plane
+clc; clear; close all;
+
+% Parámetros
+f      = 2.6e9;        % frecuencia (Hz)
+c      = 3e8;          % velocidad de la luz (m/s)
+lambda = c/f;          % longitud de onda (m)
+L      = (lambda/2);     % dipolo de longitud lambda/2
+k      = 2*pi/lambda;  % numero de onda
+
+% Malla angular
+n     = 360;
+theta = linspace(0,2*pi,n);
+phi   = linspace(0,pi,n);
+[Theta,Phi] = meshgrid(theta,phi);
+
+% Patrón E(theta,phi) normalizado
+E = abs((cos(k*L/2.*cos(Phi)) - cos(k*L/2))./(sin(Phi)+eps));
+E = E / max(E(:));
+
+% Patrón 3D
+[X,Y,Z] = sph2cart(Theta,pi/2-Phi,E);
+figure;
+surf(X,Y,Z,E,'EdgeColor','none');
+axis equal; axis off;
+colormap jet; colorbar;
+view(40,30);
+lighting gouraud; camlight headlight;
+title('3D - Dipole L=lambda/2');
+
+% H-plane (phi = pi/2)
+[~,i] = min(abs(phi - pi/2));
+Eh = E(i,:);
+figure;
+polarplot(theta,Eh,'b','LineWidth',2);
+title('H-plane (phi=pi/2) - L=lambda/2');
+
+% E-plane (theta = 0)
+[~,j] = min(abs(theta - 0));
+Ev = E(:,j);
+phi_full = [phi, phi+pi];
+Ev_full  = [Ev.', Ev.'];
+figure;
+polarplot(phi_full,Ev_full,'r','LineWidth',2);
+title('E-plane (theta=0) - L=lambda/2');
